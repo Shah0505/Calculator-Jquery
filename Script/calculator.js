9940fdbc4288010b3de2bbf86f1f839aa10392eb
Soham Shah <soham.shah21@gmail.com>
2016-09-14 15:48:53 -0500

    Updated files

--- Script/calculator.js
+++ Script/calculator.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-var temp1=" ";
+var temp1="";
 
 function stringconcate(val){
 
@@ -8,9 +8,13 @@ function stringconcate(val){
 	displaydata(temp1);
 } 
 
-function displaydata(temp1){
+function displaydata(temp1,location){
 
-	$(".display").val(temp1);
+if (location === undefined) {
+	location = "#primarydisplay";
+}
+
+	$(location).html(temp1);
 
 }
 	$(".btn").click(function(){
@@ -21,15 +25,22 @@ function displaydata(temp1){
 		});
 $(".sign.cc").click(function(){
 		displaydata(0);
+		displaydata("","#secondarydisplay");
+		temp1 = "";
 		});
 	
 $(".sign.del").click(function(){
 		temp1=temp1.slice(0,-1);
+		temp1 = (temp1 !== "") ? temp1:"0"
 		displaydata(temp1);
+		displaydata("","#secondarydisplay");
 	});
 
 $(".sign.equal").click(function(){
+		displaydata(temp1,"#secondarydisplay")
 		temp1=eval(temp1);
+		temp1 = temp1.toString();
+		console.log(temp1);
 		displaydata(temp1);
 	});
 });
